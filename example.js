3a70f80763fa3c238a559d21fb1a7b7973f9b50d
dilan2 <serglebko@gmail.com>
2018-02-13 18:04:21 +0300

    part 2

--- example.js
+++ example.js
@@ -19,9 +19,9 @@ function api_generator(method, url) {
 
     return function() {
 	ajax(url, method, this.data);
+	this.show_data();
     }
 }
-
 
 var get_reviews = api_generator('GET', '/get_reviews');
 var order = api_generator('POST', '/order');
@@ -29,6 +29,50 @@ var order = api_generator('POST', '/order');
 
 get_reviews(data);
 
+
+
+
+
+function generate_from_type(type) {
+    var type = type;
+
+
+    function Test() {
+	this.method = function() {
+	    console.log(this.data);
+	}
+    }
+    
+
+    return function() {
+	this['ajax_' + type](this.data);
+
+	var res = new Test();
+
+	return Test;
+	return res.mehod.call(this);
+    }
+}
+
+
+
+var iii = new generate_from_type('json')();
+
+var h = iii.bind(new FormGenerator('POST', order));
+
+var x = generate_from_type('json');
+var y = generate_from_type('yml');
+x();
+y();
+
+
+var callback = generate_from_type(json);
+
+function Test(arg,  callback) {
+    callback(arg);
+    generate_from_type('json')(arg);
+    
+}
 
 
 function FormGenerator(form, link_to_order_func) {
@@ -40,6 +84,15 @@ function FormGenerator(form, link_to_order_func) {
 
     this.test = function() {
 	console.log(this);
+    }
+
+
+    this.show_data = function() {
+	console.log(this.data);
+    }
+
+    this.ajax_json = function() {
+	ajax()
     }
 }
 
@@ -54,3 +107,6 @@ form_1.test();
 form_2.test();
 
 form_1.send();// SENDING DATA...
+
+
+
